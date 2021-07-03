622fafa50dcb2d63cbc13799b7bdf4098414f288
Flavio Dias <flavio.goncalvesdias@gmail.com>
2021-07-03 10:47:07 -0300

    Exercicio 02 Selenium

--- src/test/java/exerciciosSelenium/ExerciciosSelenium.java
+++ src/test/java/exerciciosSelenium/ExerciciosSelenium.java
@@ -1,23 +1,41 @@
 package exerciciosSelenium;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ExerciciosSelenium {
-	
+	static ChromeDriver driver = new ChromeDriver(); // Criando um chrome driver
+
+	@BeforeClass
+	public static void beforeClass() {
+		driver.get("http://demo.automationtesting.in/Register.html");// Abrindo uma pagina
+
+	}
+
 	@Test
 	public void exercicio01Selenium() {
-		ChromeDriver driver = new ChromeDriver(); //Criando um chrome driver
-		driver.get("http://demo.automationtesting.in/Register.html");//Abrindo uma pagina
-		driver.findElement(By.xpath("//*[@ng-model='FirstName']")).sendKeys("Flávio"); //Localizando elemento e enviando um texto 
-		driver.findElement(By.xpath("//*[@ng-model='LastName']")).sendKeys("Lelis Dias"); //Localizando elemento e enviando um texto 
-		driver.findElement(By.xpath("//*[@ng-model='Adress']")).sendKeys("Av Paulista, 509 - 18º andar SL 1"); //Localizando elemento e enviando um texto 
-		driver.findElement(By.xpath("//*[@ng-model='EmailAdress']")).sendKeys("contato@qaacademy.com.br"); //Localizando elemento e enviando um texto 
-		driver.findElement(By.xpath("//*[@ng-model='Phone']")).sendKeys("11977413512"); //Localizando elemento e enviando um texto 
 
-		
+		driver.findElement(By.xpath("//*[@ng-model='FirstName']")).sendKeys("1231231321321321321321"); // Localizando
+																										// elemento e
+		driver.findElement(By.xpath("//*[@ng-model='LastName']")).sendKeys("Lelis Dias"); // Localizando elemento e
+																							// enviando um texto
+		driver.findElement(By.xpath("//*[@ng-model='Adress']")).sendKeys("Av Paulista, 509 - 18º andar SL 1"); // Localizando
+																												// elemento
+																												// texto
+		driver.findElement(By.xpath("//*[@ng-model='EmailAdress']")).sendKeys("contato@qaacademy.com.br"); // Localizando
+																											// elemento
+																											// um texto
+		driver.findElement(By.xpath("//*[@ng-model='Phone']")).sendKeys("11977413512"); // Localizando elemento e
+																						// enviando um texto
+
+	}
+
+	@Test
+	public void exercicio02Selenium() {
+		driver.findElement(By.xpath("//*[@value='Male']")).click();
 	}
 
 }
